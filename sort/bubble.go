030dd173c3cf088d56d10324730751a0ba17248f
militska <cheshirenok@gmail.com>
2021-05-16 23:37:55 +0500

    Сортировка пузырьком, golang

--- sort/bubble.go
+++ sort/bubble.go
@@ -5,38 +5,26 @@ import "fmt"
 func main() {
 	fmt.Print("hi! \n")
 	x := []int8{3, 20, 5, 32, 1, 16, 98, 76, 2}
-	var iteration int = 0
 
-	sortArray := sortBubble(x, iteration)
+	sortArray := sortBubble(x)
 	fmt.Print(sortArray)
 }
 
-func sortBubble(x []int8, iteration int) []int8 {
-	for {
-		needRepeat := false
-		internalIter := 0
+func sortBubble(x []int8) []int8 {
+	needRepeat := true
+	for needRepeat {
+		needRepeat = false
 		for i, v := range x {
-			preview := x[i]
+			prevKey := i
 			if i != 0 {
-				preview = x[i-1]
-				if v > preview {
-					internalIter += 1
-					needRepeat = true
-					x[i], x[i-1] = x[i-1], v
-				}
+				prevKey = i - 1
 			}
-		}
-		if internalIter != 0 {
-			iteration += 1
-		}
-		fmt.Printf("проход : %d \n", iteration)
+			if v < x[prevKey] {
+				needRepeat = true
+				x[i], x[i-1] = x[i-1], v
+			}
 
-		if needRepeat == true {
-			continue
 		}
-		break
 	}
-
-	fmt.Printf("Количество проходов : %d \n", iteration)
 	return x
 }
